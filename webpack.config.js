bbe89fcd16aa2acdeaf939e0667f3018c7f7d867
shakilkumar.r <shakilkumar.r@zohocorp.com>
2019-02-21 21:31:29 +0530

    prod mode

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,6 @@ module.exports = {
     entry: [
         './src/index.js'
     ],
-    mode : "development",
     output: {
         path: path.resolve(__dirname, 'bin'),
         filename: 'app.js'
